dc9afc3d0e4bf884e5a1ac7c2b4d7f16e2d4c946
Luc Brewin <luc.coolbrew@gmail.com>
2024-09-12 13:44:24 +0200

    made it so the artworks are added dynamically to the html page

---
+++ artwork.js
@@ -0,0 +1,110 @@
+const artworks = [
+  {
+    id: 1,
+    title: "The Viewer's Condition",
+    images: [
+      { src: "assets/artwork 1.1.jpg", alt: "The Viewer's Condition - 1" },
+      { src: "assets/artwork 1.2.jpg", alt: "The Viewer's Condition - 2" },
+      { src: "assets/artwork 1.3.jpg", alt: "The Viewer's Condition - 3" },
+    ],
+    link: "artwork1.html",
+  },
+  {
+    id: 2,
+    title: "White Cube Sculpture Series",
+    images: [
+      { src: "assets/artwork 2.jpg", alt: "White Cube Sculpture Series" },
+    ],
+    link: "artwork2.html",
+  },
+  {
+    id: 3,
+    title: "Terraforming the Gallery Space",
+    images: [
+      {
+        src: "assets/artwork 3.1.jpg",
+        alt: "Terraforming the Gallery Space n°2",
+      },
+      {
+        src: "assets/artwork 3.2.jpg",
+        alt: "Terraforming the Gallery Space n°1",
+      },
+    ],
+    link: "artwork3.html",
+  },
+  {
+    id: 4,
+    title: "Portrait",
+    images: [{ src: "assets/artwork 4.jpg", alt: "Portrait" }],
+    link: "artwork4.html",
+  },
+  {
+    id: 5,
+    title: "Landscape",
+    images: [{ src: "assets/artwork 5.jpg", alt: "Landscape" }],
+    link: "artwork5.html",
+  },
+  {
+    id: 6,
+    title: "A Room",
+    images: [{ src: "assets/artwork 6.jpg", alt: "A Room" }],
+    link: "artwork6.html",
+  },
+  {
+    id: 7,
+    title: "The Plant",
+    images: [{ src: "assets/artwork 7.jpg", alt: "The Plant" }],
+    link: "artwork7.html",
+  },
+  {
+    id: 8,
+    title: "The Politician",
+    images: [{ src: "assets/artwork 8.jpg", alt: "The Politician" }],
+    link: "artwork8.html",
+  },
+  {
+    id: 9,
+    title: "Untitled",
+    images: [
+      { src: "assets/artwork 9.1.jpg", alt: "Untitled" },
+      { src: "assets/artwork 9.2.jpg", alt: "Untitled" },
+    ],
+    link: "artwork9.html",
+  },
+  {
+    id: 10,
+    title: "Blue Canvas",
+    images: [{ src: "assets/artwork 10.jpg", alt: "Blue Canvas" }],
+    link: "artwork10.html",
+  },
+  {
+    id: 11,
+    title: "Untitled",
+    images: [{ src: "assets/artwork 11.jpg", alt: "Untitled" }],
+    link: "artwork11.html",
+  },
+  {
+    id: 12,
+    title: "Blue Lines",
+    images: [
+      { src: "assets/artwork 12.1.jpg", alt: "Blue Lines 1" },
+      { src: "assets/artwork 12.2.jpg", alt: "Blue Lines 2" },
+    ],
+    link: "artwork12.html",
+  },
+  {
+    id: 13,
+    title: "Why me? Why them?",
+    images: [
+      { src: "assets/artwork 13.1.jpg", alt: "Why me?" },
+      { src: "assets/artwork 13.2.jpg", alt: "Why them?" },
+    ],
+    link: "artwork13.html",
+  },
+  {
+    id: 14,
+    title: "F. Bacon Portrait",
+    images: [{ src: "assets/artwork 14.jpg", alt: "F. Bacon Portrait" }],
+    link: "artwork14.html",
+  },
+];
